1132c5b96f62f9f6fa6b022258d1c4a270e1e8cd
Ahmed Mashaal <46572957+mashal99@users.noreply.github.com>
2024-10-02 23:06:12 -0400

    added test_operations to run test using faker

---
+++ tests/test_operations.py
@@ -0,0 +1,6 @@
+from calculator.operations import add, subtract, multiply, divide
+
+def test_operation(a, b, operation, expected):
+    '''Test different operations using dynamically generated test data'''
+    result = operation(a, b)
+    assert result == expected, f"Expected {expected} but got {result} for {operation.__name__} with {a}, {b}"
